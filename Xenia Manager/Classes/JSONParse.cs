0dbb7e174c481adf4fc280606cd5c8a0e689cd7b
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-25 19:29:48 +0200

    Added classes for JSON parsing
    - Added a class for GameInfo (Will be used for parsing the .JSON file with games and their box arts) and CoverDetails for extra JSON attributes
    - Added a class GamePatch (Will be used to parse .JSON file that holds all of the patches for games
    - Added necessary libraries for project

---
+++ Xenia Manager/Classes/JSONParse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// Imported via NuGet
+using Newtonsoft.Json;
+
+namespace Xenia_Manager.Classes
+{
+    /// <summary>
+    /// This is used to parse JSON files that have game names and their box arts
+    /// </summary>
+    public class GameInfo
+    {
+        // This is for Andy Declari's JSON file
+        [JsonProperty("Name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("Front")]
+        public CoverDetails? Front { get; set; }
+
+        [JsonProperty("Back")]
+        public CoverDetails? Back { get; set; }
+
+        // This is for Wikipedia JSON file
+        [JsonProperty("Title")]
+        public string? Title { get; set; }
+
+        [JsonProperty("Link")]
+        public string? Link { get; set; }
+
+        [JsonProperty("Image URL")]
+        public string? ImageUrl { get; set; }
+    }
+
+    /// <summary>
+    /// This is for CoverDetails (Andy Declari's JSON file)
+    /// </summary>
+    public class CoverDetails
+    {
+        [JsonProperty("Full Size")]
+        public string? FullSize { get; set; }
+
+        [JsonProperty("Thumbnail")]
+        public string? Thumbnail { get; set; }
+    }
+
+    /// <summary>
+    /// This is used to parse a JSON file that holds all of the Game Patches for Xenia
+    /// </summary>
+    public class GamePatch
+    {
+        [JsonProperty("name")]
+        public required string gameName { get; set; }
+
+        [JsonProperty("download_url")]
+        public required string url { get; set; }
+    }
+}
