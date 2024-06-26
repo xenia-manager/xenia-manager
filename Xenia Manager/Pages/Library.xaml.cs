8ffc2d9e10f032ba25e8ec4d9d3a6bb8841e17af
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-26 21:31:50 +0200

    Added game detection when importing
    - When adding game, it'll launch Xenia to find game name and it's id

--- Xenia Manager/Pages/Library.xaml.cs
+++ Xenia Manager/Pages/Library.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+// Imported
+using Microsoft.Win32;
+using Serilog;
 
 namespace Xenia_Manager.Pages
 {
@@ -25,9 +20,91 @@ namespace Xenia_Manager.Pages
             InitializeComponent();
         }
 
-        private void AddGame_Click()
+        /// <summary>
+        /// Used to get game title from Xenia Window Title
+        /// </summary>
+        /// <param name="selectedFilePath">Where the selected game file is (.iso etc.)</param>
+        private async Task GetGameTitle(string selectedFilePath)
         {
+            try
+            {
+                Log.Information("Launching game with Xenia to find the name of the game");
+                Process xenia = new Process();
+                xenia.StartInfo.FileName = App.appConfiguration.EmulatorLocation + "xenia_canary.exe";
+                xenia.StartInfo.Arguments = $@"""{selectedFilePath}""";
+                xenia.Start();
+                xenia.WaitForInputIdle();
 
+                string gameTitle = "";
+                string game_id = "";
+
+                Process process = Process.GetProcessById(xenia.Id);
+                Log.Information("Trying to find the game title from Xenia Window Title");
+                int NumberOfTries = 0;
+                while (gameTitle == "" || gameTitle == "Not found")
+                {
+                    Regex titleRegex = new Regex(@"\]\s+(.+)\s+<");
+                    Regex idRegex = new Regex(@"\[([A-Z0-9]+)\s+v\d+\.\d+\]");
+
+                    Match gameNameMatch = titleRegex.Match(process.MainWindowTitle);
+                    gameTitle = gameNameMatch.Success ? gameNameMatch.Groups[1].Value : "Not found";
+                    Match versionMatch = idRegex.Match(process.MainWindowTitle);
+                    game_id = versionMatch.Success ? versionMatch.Groups[1].Value : "Not found";
+
+                    process = Process.GetProcessById(xenia.Id);
+                    NumberOfTries++;
+                    if (NumberOfTries > 100)
+                    {
+                        gameTitle = "Not found";
+                        game_id = "Not found";
+                        break;
+                    }
+                    await Task.Delay(100);
+                }
+
+                xenia.CloseMainWindow();
+                xenia.Close();
+                xenia.Dispose();
+
+                Log.Information("Game found");
+                Log.Information("Game Title: " + gameTitle);
+                Log.Information("Game ID: " + game_id);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message + "\nFull Error:\n" + ex);
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Opens FileDialog where user selects the game
+        /// </summary>
+        private async void AddGame_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Open file dialog
+                Log.Information("Open file dialog");
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Title = "Select a game";
+                openFileDialog.Filter = "Supported Files|*.iso;*.xex;*.zar|ISO Files (*.iso)|*.iso|XEX Files (*.xex)|*.xex|ZAR Files (*.zar)|*.zar|All Files|*";
+                bool? result = openFileDialog.ShowDialog();
+                if (result == true)
+                {
+                    Log.Information($"Selected file: {openFileDialog.FileName}");
+                    await GetGameTitle(openFileDialog.FileName);
+                }
+                //await LoadGames();
+                //await SaveGames();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message + "\nFull Error:\n" + ex);
+                MessageBox.Show(ex.Message);
+                return;
+            }
         }
     }
 }
