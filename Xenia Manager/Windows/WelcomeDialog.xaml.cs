2ee3d633e2ebc6472507fb7bb265cc8e96dc3ae6
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-26 15:19:20 +0200

    Added Welcome Window
    - Added welcome window that will show up if Xenia isn't installed or at first launch
    - Changed the Title of the Main Window
    - Added a fade in animation to the Main Window

---
+++ Xenia Manager/Windows/WelcomeDialog.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Xenia_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for WelcomeDialog.xaml
+    /// </summary>
+    public partial class WelcomeDialog : Window
+    {
+        public WelcomeDialog()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(1000);
+            this.Topmost = false;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void InstallXeniaCanary_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
