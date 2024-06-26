5b473babab71112d1229aa32bf36f36014036445
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-26 21:42:02 +0200

    Added SelectGame window
    - Added SelectGame window to select a game from our "database" so the Manager can use proper boxart for the game

---
+++ Xenia Manager/Windows/SelectGame.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Xenia_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for SelectGame.xaml
+    /// </summary>
+    public partial class SelectGame : Window
+    {
+        public SelectGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
