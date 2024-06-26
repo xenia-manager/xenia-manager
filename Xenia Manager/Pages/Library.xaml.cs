164b5f77551ad0e3fb01d3ff7485f069dffd5c5f
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-26 21:16:42 +0200

    Added a Library Page
    - Added a default Library page where all games will be shown

---
+++ Xenia Manager/Pages/Library.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Xenia_Manager.Pages
+{
+    /// <summary>
+    /// Interaction logic for Library.xaml
+    /// </summary>
+    public partial class Library : Page
+    {
+        public Library()
+        {
+            InitializeComponent();
+        }
+
+        private void AddGame_Click()
+        {
+
+        }
+    }
+}
