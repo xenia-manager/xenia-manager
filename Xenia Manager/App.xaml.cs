8e723384b19ad73af5bf560e413950254a22c238
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-26 16:02:30 +0200

    Added a logger
    - Added Serilog logger that starts on startup in the Application_Startup Function
    - Added a way to delete old log files (older than a week)
    - Added AllocConsole for showing console if -console argument was added to the shortcut of the Xenia Manager executable

--- Xenia Manager/App.xaml.cs
+++ Xenia Manager/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
+
+// Imported
+using Serilog;
 
 namespace Xenia_Manager
 {
@@ -9,6 +13,51 @@ namespace Xenia_Manager
     /// </summary>
     public partial class App : Application
     {
-    }
+        // This is needed for Console to show up when using argument -console
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
 
+        /// <summary>
+        /// This function is used to delete old log files (older than a week)
+        /// </summary>
+        /// <param name="logDirectory"></param>
+        /// <param name="retentionPeriod"></param>
+        private void CleanUpOldLogFiles(string logDirectory, TimeSpan retentionPeriod)
+        {
+            string[] logFiles = Directory.GetFiles(logDirectory, "Log-*.txt");
+            DateTime currentTime = DateTime.UtcNow;
+
+            foreach (string logFile in logFiles)
+            {
+                FileInfo fileInfo = new FileInfo(logFile);
+                if (fileInfo.CreationTimeUtc < currentTime - retentionPeriod)
+                {
+                    fileInfo.Delete();
+                }
+            }
+        }
+
+        /// <summary>
+        /// This function holds everything that happens when the application is launching
+        /// </summary>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            CleanUpOldLogFiles("Logs", TimeSpan.FromDays(7));
+            // Initializing Logger
+            Serilog.Log.Logger = Log.Logger;
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(
+                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss}|{Level}|{Message}{Exception}")
+                //.WriteTo.File("Logs/Log-.txt", rollingInterval: RollingInterval.Day) - Uncomment this line to save logs into a file
+                .CreateLogger();
+
+            // Checks for all of the Launch Arguments
+            if (e.Args.Contains("-console"))
+            {
+                AllocConsole();
+            }
+
+            Log.Information("Application is running");
+        }
+    }
 }
