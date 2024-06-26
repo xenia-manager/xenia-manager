9d0fc53da8df0a3743a6a8fbea8cefecdac36abe
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-26 16:44:58 +0200

    Added loading of configuration
    - Added a function for loading configuration file. If it's not found, Welcome window will be shown.

--- Xenia Manager/App.xaml.cs
+++ Xenia Manager/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Windows;
 
 // Imported
 using Serilog;
+using Newtonsoft.Json;
+using Xenia_Manager.Classes;
+using Xenia_Manager.Windows;
 
 namespace Xenia_Manager
 {
@@ -16,6 +19,8 @@ namespace Xenia_Manager
         // This is needed for Console to show up when using argument -console
         [DllImport("Kernel32")]
         public static extern void AllocConsole();
+
+        public static Configuration? appConfiguration;
 
         /// <summary>
         /// This function is used to delete old log files (older than a week)
@@ -38,16 +43,51 @@ namespace Xenia_Manager
         }
 
         /// <summary>
+        /// Function that loads the configuration file for Xenia Manager
+        /// </summary>
+        private async Task LoadConfigurationFile()
+        {
+            try
+            {
+                Log.Information("Trying to load configuration file");
+                string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
+
+                if (File.Exists(configPath))
+                {
+                    string json = await File.ReadAllTextAsync(configPath).ConfigureAwait(false);
+                    appConfiguration = JsonConvert.DeserializeObject<Configuration>(json);
+                    Log.Information("Configuration file loaded.");
+                }
+                else
+                {
+                    Log.Warning("Configuration file not found. (Could be fresh install)");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while loading the configuration file");
+                MessageBox.Show($"{ex.Message}\nFull Error:\n{ex}");
+            }
+        }
+
+        /// <summary>
         /// This function holds everything that happens when the application is launching
         /// </summary>
-        private void Application_Startup(object sender, StartupEventArgs e)
+        private async void Application_Startup(object sender, StartupEventArgs e)
         {
-            CleanUpOldLogFiles("Logs", TimeSpan.FromDays(7));
+            if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "Logs"))
+            {
+                Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Logs");
+            }
+
+            // Clean old logs (Older than 7 days)
+            CleanUpOldLogFiles(AppDomain.CurrentDomain.BaseDirectory + "Logs", TimeSpan.FromDays(7));
+
             // Initializing Logger
             Serilog.Log.Logger = Log.Logger;
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console(
-                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss}|{Level}|{Message}{Exception}")
+                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss}|{Level}|{Message}{NewLine}{Exception}")
                 //.WriteTo.File("Logs/Log-.txt", rollingInterval: RollingInterval.Day) - Uncomment this line to save logs into a file
                 .CreateLogger();
 
@@ -57,6 +97,24 @@ namespace Xenia_Manager
                 AllocConsole();
             }
 
+            // Load the configuration file for Xenia Manager
+            await LoadConfigurationFile();
+
+            // Checking if there is a configuration file
+            if (appConfiguration != null)
+            {
+                // If there is a configuration file, check if it already checked for an update
+                if (appConfiguration.LastUpdateCheckDate == null || (DateTime.Now - appConfiguration.LastUpdateCheckDate.Value).TotalDays >= 1)
+                {
+                    // Here goes check for updates
+                }
+            }
+            else
+            {
+                // If there isn't configuration file, load Welcome Window
+                WelcomeDialog welcome = new WelcomeDialog();
+                welcome.Show();
+            }
             Log.Information("Application is running");
         }
     }
