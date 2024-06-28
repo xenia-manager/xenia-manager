461dabbd7d7b24605e64aa8f89e1bec80727d430
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-28 19:30:49 +0200

    Cleaned up unused libraries

--- Xenia Manager/Windows/SelectGamePatch.xaml.cs
+++ Xenia Manager/Windows/SelectGamePatch.xaml.cs
@@ -1,21 +1,13 @@
-﻿using ImageMagick;
-using Newtonsoft.Json;
-using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
+// Imported
+using Newtonsoft.Json;
+using Serilog;
 using Xenia_Manager.Classes;
 
 namespace Xenia_Manager.Windows
