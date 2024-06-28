d5b5c6b5ce46bd1f374e7e9a9ec80cb55a3ed674
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-28 19:50:38 +0200

    Added importing of patches into the UI

--- Xenia Manager/Windows/EditGamePatch.xaml.cs
+++ Xenia Manager/Windows/EditGamePatch.xaml.cs
@@ -1,21 +1,58 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
+using System.IO;
 
 // Imported
 using Serilog;
+using Tomlyn;
+using Tomlyn.Model;
 using Xenia_Manager.Classes;
 
 namespace Xenia_Manager.Windows
 {
     /// <summary>
+    /// Patch class used to read and edit patches
+    /// </summary>
+    public class Patch
+    {
+        /// <summary>
+        /// Name of the patch
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Disabled/Enabled patch
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Explains what patch does, can be null
+        /// </summary>
+        public string? Description { get; set; }
+    }
+
+    /// <summary>
     /// Interaction logic for EditGamePatch.xaml
     /// </summary>
     public partial class EditGamePatch : Window
     {
-        // Used to send a signal that this window has been closed
-        private TaskCompletionSource<bool> _closeTaskCompletionSource = new TaskCompletionSource<bool>();
+        /// <summary>
+        /// Holds every patch as a Patch class
+        /// </summary>
+        public ObservableCollection<Patch> Patches = new ObservableCollection<Patch>();
+
+        /// <summary>
+        /// Used to send a signal that this window has been closed
+        /// </summary>
+        public TaskCompletionSource<bool> _closeTaskCompletionSource = new TaskCompletionSource<bool>();
+
+        /// <summary>
+        /// Location to the game specific patch
+        /// </summary>
+        private string patchFilePath { get; set; }
 
         /// <summary>
         /// Constructor of this window 
@@ -24,9 +61,48 @@ namespace Xenia_Manager.Windows
         public EditGamePatch(InstalledGame selectedGame)
         {
             InitializeComponent();
+            this.DataContext = this;
             InitializeAsync();
-            this.Title = $"Xenia Manager - Edit {selectedGame.Title} Patch";
+            ReadGamePatch();
+            this.Title = $"Xenia Manager - Editing {selectedGame.Title} Patch";
+            this.patchFilePath = selectedGame.PatchFilePath;
             Closed += (sender, args) => _closeTaskCompletionSource.TrySetResult(true);
+        }
+
+        /// <summary>
+        /// Reads game patch into the Patches ObservableCollection
+        /// </summary>
+        private void ReadGamePatch()
+        {
+            try
+            {
+                if (File.Exists(patchFilePath))
+                {
+                    Patches.Clear();
+                    string content = File.ReadAllText(patchFilePath);
+                    TomlTable model = Toml.ToModel(content);
+                    Log.Information($"Game name: {model["title_name"].ToString()}");
+                    Log.Information($"Game ID: {model["title_id"].ToString()}");
+                    TomlTableArray patches = model["patch"] as TomlTableArray;
+                    foreach (var patch in patches)
+                    {
+                        Patch newPatch = new Patch();
+                        newPatch.Name = patch["name"].ToString();
+                        newPatch.IsEnabled = bool.Parse(patch["is_enabled"].ToString());
+                        if (patch.ContainsKey("desc"))
+                        {
+                            newPatch.Description = patch["desc"].ToString();
+                        }
+                        Patches.Add(newPatch);
+                    }
+                }
+                ListOfPatches.ItemsSource = Patches;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message + "\nFull Error:\n" + ex);
+                MessageBox.Show(ex.Message);
+            }
         }
 
         /// <summary>
@@ -40,6 +116,7 @@ namespace Xenia_Manager.Windows
                 {
                     this.Visibility = Visibility.Hidden;
                     Mouse.OverrideCursor = Cursors.Wait;
+                    ReadGamePatch();
                 });
             }
             catch (Exception ex)
