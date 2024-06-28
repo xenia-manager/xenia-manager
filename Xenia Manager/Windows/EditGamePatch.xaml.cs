1cf6b4935cf95ac7006c1b1c907ed0a20fd75473
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-28 19:21:12 +0200

    Added EditGamePatch window

---
+++ Xenia Manager/Windows/EditGamePatch.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Xenia_Manager.Classes;
+
+namespace Xenia_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for EditGamePatch.xaml
+    /// </summary>
+    public partial class EditGamePatch : Window
+    {
+        /// <summary>
+        /// Constructor of this window 
+        /// </summary>
+        /// <param name="selectedGame">Game whose patch will be edited</param>
+        public EditGamePatch(InstalledGame selectedGame)
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Exits this window
+        /// </summary>
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
