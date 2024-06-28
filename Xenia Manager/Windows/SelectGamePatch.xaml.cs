005bf91ccf4c333dfad2f4c7f8e3d32378e8d5ad
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-28 15:16:50 +0200

    Added window to download patch for a game

---
+++ Xenia Manager/Windows/SelectGamePatch.xaml.cs
@@ -0,0 +1,76 @@
+﻿using ImageMagick;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Xenia_Manager.Classes;
+
+namespace Xenia_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for SelectGamePatch.xaml
+    /// </summary>
+    public partial class SelectGamePatch : Window
+    {
+        // We store the selected game here
+        private InstalledGame selectedGame;
+
+        public SelectGamePatch()
+        {
+            InitializeComponent();
+        }
+
+        public SelectGamePatch(InstalledGame game)
+        {
+            InitializeComponent();
+            this.selectedGame = game;
+        }
+
+        private async void InitializeAsync()
+        {
+            try
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    this.Visibility = Visibility.Hidden;
+                    Mouse.OverrideCursor = Cursors.Wait;
+                });
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message + "\nFull Error:\n" + ex);
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    this.Visibility = Visibility.Visible;
+                    Mouse.OverrideCursor = null;
+                });
+
+            }
+        }
+
+        /// <summary>
+        /// Closes this window
+        /// </summary>
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Log.Information("Closing SelectGamePatch window");
+            this.Close();
+        }
+    }
+}
