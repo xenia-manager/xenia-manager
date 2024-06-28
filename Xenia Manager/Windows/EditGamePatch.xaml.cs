1e54a4a1b993d5e0c378a1b6b90daf798d87c299
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-06-28 19:29:21 +0200

    Added Edit Game Patch to the ContextMenu

--- Xenia Manager/Windows/EditGamePatch.xaml.cs
+++ Xenia Manager/Windows/EditGamePatch.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Xenia_Manager.Classes;
@@ -20,6 +22,9 @@ namespace Xenia_Manager.Windows
     /// </summary>
     public partial class EditGamePatch : Window
     {
+        // Used to send a signal that this window has been closed
+        private TaskCompletionSource<bool> _closeTaskCompletionSource = new TaskCompletionSource<bool>();
+
         /// <summary>
         /// Constructor of this window 
         /// </summary>
@@ -27,6 +32,53 @@ namespace Xenia_Manager.Windows
         public EditGamePatch(InstalledGame selectedGame)
         {
             InitializeComponent();
+            InitializeAsync();
+            this.Title = $"Xenia Manager - Edit {selectedGame.Title} Patch";
+            Closed += (sender, args) => _closeTaskCompletionSource.TrySetResult(true);
+        }
+
+        /// <summary>
+        /// Function that executes other functions asynchronously
+        /// </summary>
+        private async void InitializeAsync()
+        {
+            try
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    this.Visibility = Visibility.Hidden;
+                    Mouse.OverrideCursor = Cursors.Wait;
+                });
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message + "\nFull Error:\n" + ex);
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    this.Visibility = Visibility.Visible;
+                    Mouse.OverrideCursor = null;
+                });
+
+            }
+        }
+
+        /// <summary>
+        /// Used to execute fade in animation when loading is finished
+        /// </summary>
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (this.Visibility == Visibility.Visible)
+            {
+                Storyboard fadeInStoryboard = this.FindResource("FadeInStoryboard") as Storyboard;
+                if (fadeInStoryboard != null)
+                {
+                    fadeInStoryboard.Begin(this);
+                }
+            }
         }
 
         /// <summary>
@@ -34,7 +86,17 @@ namespace Xenia_Manager.Windows
         /// </summary>
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
+            Log.Information("Closing EditGamePatch window");
             this.Close();
+        }
+
+        /// <summary>
+        /// Used to emulate a WaitForCloseAsync function that is similar to the one Process Class has
+        /// </summary>
+        /// <returns></returns>
+        public Task WaitForCloseAsync()
+        {
+            return _closeTaskCompletionSource.Task;
         }
     }
 }
