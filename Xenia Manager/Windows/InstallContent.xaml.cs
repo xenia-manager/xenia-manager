fae963b98646a4b72833fcf7e30d54402fcace58
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-07-19 14:21:53 +0200

    Added InstallContent window
    - This will be used to show everything user wants to install before actually installing it

---
+++ Xenia Manager/Windows/InstallContent.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Xenia_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for InstallContent.xaml
+    /// </summary>
+    public partial class InstallContent : Window
+    {
+        public InstallContent()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
