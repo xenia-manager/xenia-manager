284f2ab00fb9e52a9b302e0ac246bd8243a991fe
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-07-19 19:34:38 +0200

    Update InstallContent.xaml.cs
    - Implemented waitforexitasync and async initialization
    - Added function that reads and adds content to the listbox

--- Xenia Manager/Windows/InstallContent.xaml.cs
+++ Xenia Manager/Windows/InstallContent.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+// Imported
+using Serilog;
+using Xenia_Manager.Classes;
 
 namespace Xenia_Manager.Windows
 {
@@ -20,14 +14,134 @@ namespace Xenia_Manager.Windows
     /// </summary>
     public partial class InstallContent : Window
     {
-        public InstallContent()
+        List<GameContent> gameContent = new List<GameContent>();
+
+        // Used to send a signal that this window has been closed
+        private TaskCompletionSource<bool> _closeTaskCompletionSource = new TaskCompletionSource<bool>();
+
+        public InstallContent(List<GameContent> gameContent)
         {
             InitializeComponent();
+            this.gameContent = gameContent;
+            InitializeAsync();
+            Closed += (sender, args) => _closeTaskCompletionSource.TrySetResult(true);
         }
 
-        private void Exit_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Used to read the content from the list into the UI (ListBox)
+        /// </summary>
+        private async Task ReadContent()
         {
-            this.Close();
+            try
+            {
+                foreach (GameContent content in gameContent)
+                {
+                    string test = "";
+                    if (!content.ContentDisplayName.Contains(content.ContentTitle) && content.ContentTitle != "")
+                    {
+                        test += $"{content.ContentTitle} ";
+                    }
+                    test += $"{content.ContentDisplayName} ";
+                    test += $"({content.ContentType})";
+                    ListOfContentToInstall.Items.Add(test);
+                }
+                await Task.Delay(1);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message + "\nFull Error:\n" + ex);
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Function that executes other functions asynchronously
+        /// </summary>
+        private async void InitializeAsync()
+        {
+            try
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    this.Visibility = Visibility.Hidden;
+                    Mouse.OverrideCursor = Cursors.Wait;
+                });
+                await ReadContent();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message + "\nFull Error:\n" + ex);
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    this.Visibility = Visibility.Visible;
+                    Mouse.OverrideCursor = null;
+                });
+            }
+        }
+
+        /// <summary>
+        /// Used to execute fade in animation when loading is finished
+        /// </summary>
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (this.Visibility == Visibility.Visible)
+            {
+                Storyboard fadeInStoryboard = ((Storyboard)Application.Current.FindResource("FadeInAnimation")).Clone();
+                if (fadeInStoryboard != null)
+                {
+                    fadeInStoryboard.Begin(this);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Used to emulate a WaitForCloseAsync function that is similar to the one Process Class has
+        /// </summary>
+        /// <returns></returns>
+        public Task WaitForCloseAsync()
+        {
+            return _closeTaskCompletionSource.Task;
+        }
+
+        /// <summary>
+        /// Does fade out animation before closing the window
+        /// </summary>
+        private async Task ClosingAnimation()
+        {
+            Storyboard FadeOutClosingAnimation = ((Storyboard)Application.Current.FindResource("FadeOutAnimation")).Clone();
+
+            FadeOutClosingAnimation.Completed += (sender, e) =>
+            {
+                Log.Information("Closing SelectGame window");
+                this.Close();
+            };
+
+            FadeOutClosingAnimation.Begin(this);
+            await Task.Delay(1);
+        }
+
+        // UI
+        /// <summary>
+        /// Closes this window
+        /// </summary>
+        private async void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            await ClosingAnimation();
+        }
+
+        private void ListOfContentToInstall_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ListOfContentToInstall.SelectedIndex >= 0)
+            {
+                Log.Information(gameContent[ListOfContentToInstall.SelectedIndex].ContentTitle);
+                Log.Information(gameContent[ListOfContentToInstall.SelectedIndex].ContentDisplayName);
+                Log.Information(gameContent[ListOfContentToInstall.SelectedIndex].ContentType);
+                Log.Information(gameContent[ListOfContentToInstall.SelectedIndex].ContentTypeValue);
+            }
         }
     }
 }
