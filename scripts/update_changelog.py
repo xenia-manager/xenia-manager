8e804cdf5f506ada4ca8a35ccec98e2b6b8d1c75
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-07-21 11:17:14 +0200

    Update update_changelog.py

--- scripts/update_changelog.py
+++ scripts/update_changelog.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def get_releases(repo):
-    url = f"https://api.github.com/repos/{repo}/releases"
+    url = f"https://api.github.com/repos/xenia-manager/xenia-manager/releases"
     headers = {"Authorization": f"token {os.getenv('GITHUB_TOKEN')}"}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
@@ -15,15 +15,11 @@ def compile_changelog(releases):
         changelog.append(f"{release['body']}\n")
     return "\n".join(changelog)
 
-def write_changelog(changelog, file_path="CHANGELOG.txt"):
-    with open(file_path, "w") as file:
-        file.write(changelog)
-
 def main():
     repo = os.getenv('GITHUB_REPOSITORY')
     releases = get_releases(repo)
     changelog = compile_changelog(releases)
-    write_changelog(changelog)
+    print(changelog)
 
 if __name__ == "__main__":
     main()
