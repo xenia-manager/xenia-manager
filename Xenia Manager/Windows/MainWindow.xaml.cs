d55936d6bb3fd3ecae2ca7fefa5fd337f913b06a
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-08-05 14:43:06 +0200

    Implemented Maximize button
    - Also adjusted the style for the "TitleBar buttons"

--- Xenia Manager/Windows/MainWindow.xaml.cs
+++ Xenia Manager/Windows/MainWindow.xaml.cs
@@ -295,6 +295,21 @@ namespace Xenia_Manager
         }
 
         /// <summary>
+        /// Maximizes the Xenia Manager window
+        /// </summary>
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        /// <summary>
         /// Loads FadeOut animation and exits the application completely
         /// </summary>
         private async void Exit_Click(object sender, RoutedEventArgs e)
