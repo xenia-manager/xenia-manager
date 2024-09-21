da52100f12c7f9d63ec901d54daabcfd049409a4
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-09-21 17:06:19 +0200

    UI
    - Ported App.XAML and split it up into different components so it's easier to debug
    - Ported Themes and Assets and moved the default MainWindow location into another folder

--- XeniaManager/Views/MainWindow.xaml.cs
+++ XeniaManager/Views/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace XeniaManager
 {
