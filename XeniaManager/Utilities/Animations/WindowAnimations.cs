e262aede03fb9d0085e05fac2015719ad27ed42e
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-09-22 16:23:07 +0200

    Added "WindowAnimations" class

---
+++ XeniaManager/Utilities/Animations/WindowAnimations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+// Imported
+using Serilog;
+
+namespace XeniaManager.Utilities.Animations
+{
+    public static class WindowAnimations
+    {
+        /// <summary>
+        /// Do "fade-out" animation and close the window
+        /// </summary>
+        /// <param name="window">The window to animate and close</param>
+        /// <param name="customAction">Optional action to run after the animation, like closing the app or just the window</param>
+        public static void ClosingAnimation(Window window, Action? customAction = null)
+        {
+            // Fetch fadeout animation
+            Storyboard fadeOutClosingAnimation = ((Storyboard)Application.Current.FindResource("FadeOutAnimation")).Clone();
+
+            // Once it's completed, do an action
+            fadeOutClosingAnimation.Completed += (sender, e) =>
+            {
+                Log.Information($"Closing {window.Title} window");
+                if (customAction != null)
+                {
+                    customAction(); // Custom Action
+                }
+                else
+                {
+                    window.Close(); // Default behavior: close the window
+                }
+            };
+
+            fadeOutClosingAnimation.Begin(window); // Run the "fade-out" animation
+        }
+    }
+}
