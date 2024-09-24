91122738100616698f9b8bc4c3db14c92aa84491
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-09-24 11:35:48 +0200

    Added classes for interacting with Database

---
+++ XeniaManager/Database/GameInfo.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+// Imported
+using Newtonsoft.Json;
+
+namespace XeniaManager
+{
+    /// <summary>
+    /// This is used to parse games list that are stored as .JSON files
+    /// </summary>
+    public class GameInfo
+    {
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("alternative_id")]
+        public List<string> AlternativeId { get; set; }
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+
+        [JsonProperty("url")]
+        public string? Link { get; set; }
+
+        [JsonProperty("artwork")]
+        public Artwork? Artwork { get; set; }
+    }
+
+    /// <summary>
+    /// This is used to parse the "artwork" section of .JSON file
+    /// </summary>
+    public class Artwork
+    {
+        // Universal
+        [JsonProperty("boxart")]
+        public string Boxart { get; set; }
+
+        // Launchbox DB specific
+        [JsonProperty("disc")]
+        public string? Disc { get; set; }
+
+        [JsonProperty("logo")]
+        public string? Logo { get; set; }
+
+        // Xbox Marketplace specific
+        [JsonProperty("background")]
+        public string? Background { get; set; }
+
+        [JsonProperty("banner")]
+        public string? Banner { get; set; }
+
+        [JsonProperty("icon")]
+        public string? Icon { get; set; }
+
+        [JsonProperty("gallery")]
+        public List<string>? Gallery { get; set; }
+    }
+
+    /// <summary>
+    /// Xbox Marketplace game info
+    /// </summary>
+    public class Title
+    {
+        [JsonProperty("full")]
+        public string Full { get; set; }
+
+        [JsonProperty("reduced")]
+        public string Reduced { get; set; }
+    }
+
+    public class Description
+    {
+        [JsonProperty("full")]
+        public string Full { get; set; }
+
+        [JsonProperty("short")]
+        public string Short { get; set; }
+    }
+
+    public class Media
+    {
+        [JsonProperty("media_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("edition")]
+        public string Edition { get; set; }
+
+        [JsonProperty("region")]
+        public string Region { get; set; }
+    }
+
+    public class Parent
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+    public class Products
+    {
+        [JsonProperty("parent")]
+        public List<Parent> Parent { get; set; }
+
+        [JsonProperty("related")]
+        public List<object> Related { get; set; }
+    }
+}
---
+++ XeniaManager/Database/Xbox Marketplace/XboxMarketplaceClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// Imported
+using Newtonsoft.Json;
+
+namespace XeniaManager
+{
+    /// <summary>
+    /// Used to parse specific game details when it has been selected in Xbox Marketplace source
+    /// </summary>
+    public class XboxMarketplaceGameInfo
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("title")]
+        public Title Title { get; set; }
+
+        [JsonProperty("genre")]
+        public List<string> Genres { get; set; }
+
+        [JsonProperty("developer")]
+        public string Developer { get; set; }
+
+        [JsonProperty("publisher")]
+        public string Publisher { get; set; }
+
+        [JsonProperty("release_date")]
+        public string ReleaseDate { get; set; }
+
+        [JsonProperty("user_rating")]
+        public string UserRating { get; set; }
+
+        [JsonProperty("description")]
+        public Description Description { get; set; }
+
+        [JsonProperty("media")]
+        public List<Media> Media { get; set; }
+
+        [JsonProperty("artwork")]
+        public Artwork? Artwork { get; set; }
+
+        [JsonProperty("products")]
+        public Products products { get; set; }
+    }
+}
