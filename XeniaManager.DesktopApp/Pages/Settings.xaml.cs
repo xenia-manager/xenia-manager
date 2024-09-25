b546c1f218d9183dc2f68ab780fdb6e1d3fc5fd1
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-09-25 23:19:30 +0200

    Added theme selector

--- XeniaManager.DesktopApp/Pages/Settings.xaml.cs
+++ XeniaManager.DesktopApp/Pages/Settings.xaml.cs
@@ -19,12 +19,63 @@ namespace XeniaManager.DesktopApp.Pages
 
         // UI Interactions
         /// <summary>
+        /// Load selected values into the UI
+        /// </summary>
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Load the selected theme into the ui
+            foreach (ComboBoxItem theme in ThemeSelector.Items)
+            {
+                if (theme.Content.ToString() == ConfigurationManager.AppConfig.SelectedTheme)
+                {
+                    ThemeSelector.SelectedItem = theme;
+                    break;
+                }
+            }
+        }
+        /// <summary>
         /// Opens InstallXenia window where the user can install or uninstall different Xenia versions
         /// </summary>
         private void OpenXeniaInstaller_Click(object sender, RoutedEventArgs e)
         {
             InstallXenia installXenia = new InstallXenia();
             installXenia.ShowDialog();
+        }
+
+        /// <summary>
+        /// Loads the selected theme
+        /// </summary>
+        private void ThemeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Check if the selection is valid
+            if (ThemeSelector.SelectedIndex < 1)
+            {
+                return;
+            }
+
+            // Switch the theme in the configuration
+            switch (ThemeSelector.SelectedIndex)
+            {
+                case 1:
+                    ConfigurationManager.AppConfig.SelectedTheme = "Light";
+                    break;
+                case 2:
+                    ConfigurationManager.AppConfig.SelectedTheme = "Dark";
+                    break;
+                case 3:
+                    ConfigurationManager.AppConfig.SelectedTheme = "AMOLED";
+                    break;
+                case 4:
+                    ConfigurationManager.AppConfig.SelectedTheme = "Nord";
+                    break;
+                default:
+                    ConfigurationManager.AppConfig.SelectedTheme = "Default";
+                    break;
+            }
+
+            // Load the theme and save changes to the file
+            App.LoadTheme();
+            ConfigurationManager.SaveConfigurationFile();
         }
     }
 }
