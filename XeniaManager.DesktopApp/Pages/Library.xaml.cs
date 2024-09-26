2453b2e75721eb4495e08a26f884dd7140bc8cad
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-09-26 17:50:48 +0200

    Implemented SearchBox for Library
    - Also changed the design of "Add Games" button

--- XeniaManager.DesktopApp/Pages/Library.xaml.cs
+++ XeniaManager.DesktopApp/Pages/Library.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 // Imported
 using Microsoft.Win32;
@@ -126,6 +127,79 @@ namespace XeniaManager.DesktopApp.Pages
                     Log.Information($"User selected Xenia {xeniaSelection.UserSelection}");
                     AddGames(openFileDialog.FileNames, xeniaSelection.UserSelection);
                     break;
+            }
+        }
+
+        // Searchbox
+        /// <summary>
+        /// If SearchBox is focused, check if it has placeholder text and remove it and reset the foreground color
+        /// </summary>
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Search games by name")
+            {
+                textBox.Text = "";
+                textBox.Foreground = (Brush)textBox.TryFindResource("ForegroundColor"); // Change text color to normal
+            }
+        }
+
+        /// <summary>
+        /// If Searchbox is lost focus, check if it has any text and if it doesn't, apply placeholder text
+        /// </summary>
+        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Search games by name";
+                textBox.Foreground = (Brush)textBox.TryFindResource("PlaceholderText"); // Change text color to gray for placeholder
+            }
+        }
+
+        /// <summary>
+        /// Executes code only when text has been changed
+        /// </summary>
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            // Don't execute search if it has placeholder text or it's empty
+            if (textBox.Text == "Search games by name" || string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                // Reset the filter
+                if (GameLibrary != null)
+                {
+                    foreach (var child in GameLibrary.Children)
+                    {
+                        if (child is GameButton gameButton)
+                        {
+                            gameButton.Visibility = Visibility.Visible;
+                        }
+                    }
+                }
+                return;
+            }
+
+            // Grab the searchQuery
+            string searchQuery = textBox.Text.ToLower();
+
+            // Search through games
+            foreach (var child in GameLibrary.Children)
+            {
+                // Ensure the element is GameButton
+                if (child is GameButton gameButton)
+                {
+                    // Check if game title contains searchquery
+                    if (gameButton.GameTitle.ToLower().Contains(searchQuery))
+                    {
+                        gameButton.Visibility = Visibility.Visible; // Show the button if it matches
+                    }
+                    else
+                    {
+                        gameButton.Visibility = Visibility.Collapsed; // Hide it if it doesn't match
+                    }
+                }
             }
         }
     }
