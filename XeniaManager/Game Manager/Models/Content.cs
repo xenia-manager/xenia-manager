5a53bb8614f28aa7272e36bc266d8d42091ba95b
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-09-27 11:12:05 +0200

    Create Content.cs
    - This is used for adding game content and title updates

---
+++ XeniaManager/Game Manager/Models/Content.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace XeniaManager
+{
+    /// <summary>
+    /// Class that contains everything about content that is going to be installed
+    /// </summary>
+    public class GameContent
+    {
+        /// <summary>
+        /// TitleID of the game
+        /// </summary>
+        public string GameId { get; set; }
+
+        /// <summary>
+        /// Title of the content file
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Display name of the content file
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Defines the type of the selected content file
+        /// </summary>
+        public string ContentType { get; set; }
+
+        /// <summary>
+        /// Value of the ContentType
+        /// </summary>
+        public string ContentTypeValue { get; set; }
+
+        /// <summary>
+        /// Location of the content
+        /// </summary>
+        public string Location { get; set; }
+    }
+}
