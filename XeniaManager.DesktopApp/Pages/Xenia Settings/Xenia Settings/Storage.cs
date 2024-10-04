3254215def54bdce7d83934173ef2681814fe0a8
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-10-04 16:59:20 +0200

    Implemented "Storage Settings"

--- XeniaManager.DesktopApp/Pages/Xenia Settings/Xenia Settings/Storage.cs
+++ XeniaManager.DesktopApp/Pages/Xenia Settings/Xenia Settings/Storage.cs
@@ -17,7 +17,19 @@ namespace XeniaManager.DesktopApp.Pages
         /// <param name="sectionTable">Portion of .toml file dedicated to Storage Settings</param>
         private void LoadStorageSettings(TomlTable sectionTable)
         {
+            // "mount_cache" setting
+            if (sectionTable.ContainsKey("mount_cache"))
+            {
+                Log.Information($"mount_cache - {(bool)sectionTable["mount_cache"]}");
+                chkMountCache.IsChecked = (bool)sectionTable["mount_cache"];
+            }
 
+            // "mount_scratch" setting
+            if (sectionTable.ContainsKey("mount_scratch"))
+            {
+                Log.Information($"mount_scratch - {(bool)sectionTable["mount_scratch"]}");
+                chkMountScratch.IsChecked = (bool)sectionTable["mount_scratch"];
+            }
         }
     }
 }
