2c2939571f640c19f7866a823d97d1534754951c
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-10-05 14:10:30 +0200

    Implemented saving of "General settings"

--- XeniaManager.DesktopApp/Pages/Xenia Settings/Xenia Settings/General.cs
+++ XeniaManager.DesktopApp/Pages/Xenia Settings/Xenia Settings/General.cs
@@ -52,7 +52,33 @@ namespace XeniaManager.DesktopApp.Pages
         /// <param name="sectionTable">Portion of .toml file dedicated to General Settings</param>
         private void SaveGeneralSettings(TomlTable sectionTable)
         {
+            // "allow_plugins" setting
+            if (sectionTable.ContainsKey("allow_plugins"))
+            {
+                Log.Information($"allow_plugins - {chkAllowPlugins.IsChecked}");
+                sectionTable["allow_plugins"] = chkAllowPlugins.IsChecked;
+            }
 
+            // "apply_patches" setting
+            if (sectionTable.ContainsKey("apply_patches"))
+            {
+                Log.Information($"apply_patches - {chkApplyPatches.IsChecked}");
+                sectionTable["apply_patches"] = chkApplyPatches.IsChecked;
+            }
+
+            // "controller_hotkeys" setting
+            if (sectionTable.ContainsKey("controller_hotkeys"))
+            {
+                Log.Information($"controller_hotkeys - {chkControllerHotkeys.IsChecked}");
+                sectionTable["controller_hotkeys"] = chkControllerHotkeys.IsChecked;
+            }
+
+            // "discord" setting
+            if (sectionTable.ContainsKey("discord"))
+            {
+                Log.Information($"discord - {chkDiscordRPC.IsChecked}");
+                sectionTable["discord"] = chkDiscordRPC.IsChecked;
+            }
         }
     }
 }
