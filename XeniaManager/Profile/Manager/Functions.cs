437e65b164c958bce6ebf682a01f21d48de36cf6
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-12-25 13:50:28 +0100

    Update Functions.cs

--- XeniaManager/Profile/Manager/Functions.cs
+++ XeniaManager/Profile/Manager/Functions.cs
@@ -5,10 +5,27 @@ namespace XeniaManager
     public static partial class ProfileManager
     {
         /// <summary>
-        /// Used to decrypt the `Account` File
+        /// Decrypts the `Account` file to retrieve the gamertag and other profile information.
+        /// This method uses RC4 encryption with HMAC-SHA1 to decrypt the account file and extract the gamertag.
+        /// The decryption process is dependent on whether a devkit is being used, with different keys being utilized.
         /// </summary>
-        /// Ref -> https://github.com/xenia-canary/xenia-canary/blob/canary_experimental/src/xenia/kernel/xam/profile_manager.cc
-        /// <returns></returns>
+        /// <param name="accountFile">The encrypted account file (byte array) that needs to be decrypted.</param>
+        /// <param name="profile">A reference to the <see cref="GamerProfile"/> object, where the decrypted gamertag will be stored.</param>
+        /// <param name="devkit">A flag indicating whether a devkit is being used. If true, a different decryption key is used.</param>
+        /// <returns>
+        /// Returns <c>false</c> if the decryption fails at any point, or if the decrypted data does not match the expected hash.
+        /// Returns <c>true</c> if the decryption succeeds and the gamertag is extracted and assigned to the <see cref="profile"/> object.
+        /// </returns>
+        /// <remarks>
+        /// The decryption process involves the following steps:
+        /// 1. Verify that the account file has the expected minimum length.
+        /// 2. Generate the RC4 decryption key using HMAC-SHA1 based on the provided account file and key.
+        /// 3. Decrypt the account file's data using the RC4 key.
+        /// 4. Verify the integrity of the decrypted data by comparing it with a hash computed from the decrypted data.
+        /// 5. Extract the gamertag (up to 15 characters) from the decrypted data and assign it to the <see cref="profile"/> object.
+        /// </remarks>
+        /// <seealso cref="GamerProfile"/>
+        /// <seealso href="https://github.com/xenia-canary/xenia-canary/blob/canary_experimental/src/xenia/kernel/xam/profile_manager.cc">Link to the source code for further reference</seealso>
         public static bool DecryptAccountFile(byte[] accountFile, ref GamerProfile profile, bool devkit = false)
         {
             // Checking if the account file is valid
