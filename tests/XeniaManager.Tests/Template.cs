e4124eea46559a3a611b634af600350e9c08713b
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-02-07 23:38:10 +0100

    Initial Commit
    V3.0.0 (UI Update)

---
+++ tests/XeniaManager.Tests/Template.cs
@@ -0,0 +1,16 @@
+﻿namespace XeniaManager.Tests;
+
+public class Template
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+        Assert.Fail();
+    }
+}
