62ba65246d467d209f56e55dbd7af470c93b23b7
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-03-26 21:28:19 +0100

    Add window for installing Xenia

---
+++ source/XeniaManager.Desktop/Views/Pages/XeniaInstallationPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XeniaManager.Desktop.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for XeniaInstallationPage.xaml
+    /// </summary>
+    public partial class XeniaInstallationPage : Page
+    {
+        public XeniaInstallationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
