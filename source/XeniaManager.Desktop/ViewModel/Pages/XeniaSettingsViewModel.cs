902cb3ecfe581e21332c61cae1bee364a37b85aa
shazzaam7 <78475878+shazzaam7@users.noreply.github.com>
2025-05-21 09:42:02 +0200

    Moved combobox items into viewmodel ObservableCollection

---
+++ source/XeniaManager.Desktop/ViewModel/Pages/XeniaSettingsViewModel.cs
@@ -0,0 +1,103 @@
+using System.Collections.ObjectModel;
+
+namespace XeniaManager.Desktop.ViewModel.Pages;
+
+public class XeniaSettingsViewModel
+{
+    public ObservableCollection<string> AudioSystems { get; } = ["any", "nop", "sdl", "xaudio2"];
+
+    public ObservableCollection<string> InternalDisplayResolutions { get; } =
+    [
+        "640x480", "640x576", "720x480", "720x576",
+        "800x600", "848x480", "1024x768", "1152x864",
+        "1280x720", "1280x768", "1280x960", "1280x1024",
+        "1360x768", "1440x900", "1680x1050",
+        "1920x540", "1920x1080", "Custom"
+    ];
+
+    public ObservableCollection<string> GraphicsApis { get; } =
+    [
+        "any", "D3D12", "Vulkan"
+    ];
+
+    public ObservableCollection<string> D3D12RenderTargetPaths { get; } =
+    [
+        "Auto", "RTV", "ROV"
+    ];
+
+    public ObservableCollection<string> D3D12QueuePriorities { get; } =
+    [
+        "Normal", "High", "Realtime"
+    ];
+
+    public ObservableCollection<string> VulkanRenderTargetPaths { get; } =
+    [
+        "Auto", "FBO", "FSI"
+    ];
+
+    public ObservableCollection<string> PostprocessAntialiasing { get; } =
+    [
+        "None", "FXAA", "FXAA Extreme"
+    ];
+
+    public ObservableCollection<string> ScalingSharpening { get; } =
+    [
+        "Bilinear", "FidelityFX CAS", "AMD FSR"
+    ];
+
+    public ObservableCollection<string> LicenseMasks { get; } =
+    [
+        "No Licenses", "First License", "All Licenses"
+    ];
+
+    public Dictionary<int, string> Countries { get; } = new Dictionary<int, string>
+    {
+        { 1, "United Arab Emirates" }, { 2, "Albania" }, { 3, "Armenia" }, { 4, "Argentina" }, { 5, "Austria" },
+        { 6, "Australia" }, { 7, "Azerbaijan" }, { 8, "Belgium" }, { 9, "Bulgaria" },
+        { 10, "Bahrain" }, { 11, "Brunei" }, { 12, "Bolivia" }, { 13, "Brazil" }, { 14, "Belarus" },
+        { 15, "Belize" }, { 16, "Canada" }, { 18, "Switzerland" }, { 19, "Chile" },
+        { 20, "China" }, { 21, "Colombia" }, { 22, "Costa Rica" }, { 23, "Czech Republic" }, { 24, "Germany" },
+        { 25, "Denmark" }, { 26, "Dominican Republic" }, { 27, "Algeria" }, { 28, "Ecuador" },
+        { 29, "Estonia" }, { 30, "Egypt" }, { 31, "Spain" }, { 32, "Finland" }, { 33, "Faroe Islands" },
+        { 34, "France" }, { 35, "United Kingdom" }, { 36, "Georgia" }, { 37, "Greece" },
+        { 38, "Guatemala" }, { 39, "Hong Kong" }, { 40, "Honduras" }, { 41, "Croatia" }, { 42, "Hungary" },
+        { 43, "Indonesia" }, { 44, "Ireland" }, { 45, "Israel" }, { 46, "India" },
+        { 47, "Iraq" }, { 48, "Iran" }, { 49, "Iceland" }, { 50, "Italy" }, { 51, "Jamaica" },
+        { 52, "Jordan" }, { 53, "Japan" }, { 54, "Kenya" }, { 55, "Kyrgyzstan" },
+        { 56, "South Korea" }, { 57, "Kuwait" }, { 58, "Kazakhstan" }, { 59, "Lebanon" }, { 60, "Liechtenstein" },
+        { 61, "Lithuania" }, { 62, "Luxembourg" }, { 63, "Latvia" }, { 64, "Libya" },
+        { 65, "Morocco" }, { 66, "Monaco" }, { 67, "North Macedonia" }, { 68, "Mongolia" }, { 69, "Macau" },
+        { 70, "Maldives" }, { 71, "Mexico" }, { 72, "Malaysia" }, { 73, "Nicaragua" },
+        { 74, "Netherlands" }, { 75, "Norway" }, { 76, "New Zealand" }, { 77, "Oman" }, { 78, "Panama" },
+        { 79, "Peru" }, { 80, "Philippines" }, { 81, "Pakistan" }, { 82, "Poland" },
+        { 83, "Puerto Rico" }, { 84, "Portugal" }, { 85, "Paraguay" }, { 86, "Qatar" }, { 87, "Romania" },
+        { 88, "Russia" }, { 89, "Saudi Arabia" }, { 90, "Sweden" }, { 91, "Singapore" },
+        { 92, "Slovenia" }, { 93, "Slovakia" }, { 95, "El Salvador" }, { 96, "Syria" }, { 97, "Thailand" },
+        { 98, "Tunisia" }, { 99, "Turkey" }, { 100, "Trinidad and Tobago" }, { 101, "Taiwan" },
+        { 102, "Ukraine" }, { 103, "United States" }, { 104, "Uruguay" }, { 105, "Uzbekistan" },
+        { 106, "Venezuela" }, { 107, "Vietnam" }, { 108, "Yemen" }, { 109, "South Africa" }
+    };
+
+    public ObservableCollection<string> Languages { get; } =
+    [
+        "English", "Japanese/日本語", "Deutsche", "Français",
+        "Español", "Italiano", "한국어", "繁體中文",
+        "Português", "Polski", "русский", "Svenska",
+        "Türk", "Norsk", "Nederlands", "简体中文"
+    ];
+
+    public ObservableCollection<string> InputSystems { get; } =
+    [
+        "Any", "SDL2", "XInput", "Keyboard"
+    ];
+
+    public ObservableCollection<string> KeyboardModes { get; } =
+    [
+        "Disabled", "Enabled", "Passthrough"
+    ];
+
+    public ObservableCollection<int> KeyboardUserIndexes { get; } =
+    [
+        0, 1, 2, 3
+    ];
+}
--- source/XeniaManager.Desktop/Views/Pages/XeniaSettingsPage.xaml.cs
+++ source/XeniaManager.Desktop/Views/Pages/XeniaSettingsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using Microsoft.Extensions.Logging;
 using XeniaManager.Core;
 using XeniaManager.Desktop.Components;
+using XeniaManager.Desktop.ViewModel.Pages;
 
 namespace XeniaManager.Desktop.Views.Pages;
 
@@ -14,38 +15,13 @@ public partial class XeniaSettingsPage : Page
     // TODO: Xenia Settings Page
 
     // Variables
-    public Dictionary<int, string> CountryIdMap { get; } = new Dictionary<int, string>
-    {
-        { 1, "United Arab Emirates" }, { 2, "Albania" }, { 3, "Armenia" }, { 4, "Argentina" }, { 5, "Austria" },
-        { 6, "Australia" }, { 7, "Azerbaijan" }, { 8, "Belgium" }, { 9, "Bulgaria" },
-        { 10, "Bahrain" }, { 11, "Brunei" }, { 12, "Bolivia" }, { 13, "Brazil" }, { 14, "Belarus" },
-        { 15, "Belize" }, { 16, "Canada" }, { 18, "Switzerland" }, { 19, "Chile" },
-        { 20, "China" }, { 21, "Colombia" }, { 22, "Costa Rica" }, { 23, "Czech Republic" }, { 24, "Germany" },
-        { 25, "Denmark" }, { 26, "Dominican Republic" }, { 27, "Algeria" }, { 28, "Ecuador" },
-        { 29, "Estonia" }, { 30, "Egypt" }, { 31, "Spain" }, { 32, "Finland" }, { 33, "Faroe Islands" },
-        { 34, "France" }, { 35, "United Kingdom" }, { 36, "Georgia" }, { 37, "Greece" },
-        { 38, "Guatemala" }, { 39, "Hong Kong" }, { 40, "Honduras" }, { 41, "Croatia" }, { 42, "Hungary" },
-        { 43, "Indonesia" }, { 44, "Ireland" }, { 45, "Israel" }, { 46, "India" },
-        { 47, "Iraq" }, { 48, "Iran" }, { 49, "Iceland" }, { 50, "Italy" }, { 51, "Jamaica" },
-        { 52, "Jordan" }, { 53, "Japan" }, { 54, "Kenya" }, { 55, "Kyrgyzstan" },
-        { 56, "South Korea" }, { 57, "Kuwait" }, { 58, "Kazakhstan" }, { 59, "Lebanon" }, { 60, "Liechtenstein" },
-        { 61, "Lithuania" }, { 62, "Luxembourg" }, { 63, "Latvia" }, { 64, "Libya" },
-        { 65, "Morocco" }, { 66, "Monaco" }, { 67, "North Macedonia" }, { 68, "Mongolia" }, { 69, "Macau" },
-        { 70, "Maldives" }, { 71, "Mexico" }, { 72, "Malaysia" }, { 73, "Nicaragua" },
-        { 74, "Netherlands" }, { 75, "Norway" }, { 76, "New Zealand" }, { 77, "Oman" }, { 78, "Panama" },
-        { 79, "Peru" }, { 80, "Philippines" }, { 81, "Pakistan" }, { 82, "Poland" },
-        { 83, "Puerto Rico" }, { 84, "Portugal" }, { 85, "Paraguay" }, { 86, "Qatar" }, { 87, "Romania" },
-        { 88, "Russia" }, { 89, "Saudi Arabia" }, { 90, "Sweden" }, { 91, "Singapore" },
-        { 92, "Slovenia" }, { 93, "Slovakia" }, { 95, "El Salvador" }, { 96, "Syria" }, { 97, "Thailand" },
-        { 98, "Tunisia" }, { 99, "Turkey" }, { 100, "Trinidad and Tobago" }, { 101, "Taiwan" },
-        { 102, "Ukraine" }, { 103, "United States" }, { 104, "Uruguay" }, { 105, "Uzbekistan" },
-        { 106, "Venezuela" }, { 107, "Vietnam" }, { 108, "Yemen" }, { 109, "South Africa" }
-    };
+    private readonly XeniaSettingsViewModel _viewModel;
 
     public XeniaSettingsPage()
     {
         InitializeComponent();
-        this.DataContext = this;
+        _viewModel = new XeniaSettingsViewModel();
+        this.DataContext = _viewModel;
         ShowOnlyPanel(SpAudioSettings);
     }
 
