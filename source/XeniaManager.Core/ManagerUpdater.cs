16bc2331e019685da417eb81b8fce9de4110fe85
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-06-06 20:41:47 +0200

    Update ManagerUpdater.cs

--- source/XeniaManager.Core/ManagerUpdater.cs
+++ source/XeniaManager.Core/ManagerUpdater.cs
@@ -4,10 +4,19 @@ namespace XeniaManager.Core;
 
 public static class ManagerUpdater
 {
+    #region Variables
+
+    private const string _repositoryOwner = "xenia-manager";
+    private const string _repositoryName = "experimental-builds";
+
+    #endregion
+
+    #region Functions
+
     public static async Task<bool> CheckForUpdates(string currentVersion)
     {
         // TODO: Change this from experimental to user builds
-        Release latestRelease = await Github.GetLatestRelease("xenia-manager", "experimental-builds");
+        Release latestRelease = await Github.GetLatestRelease(_repositoryOwner, _repositoryName);
         Logger.Debug($"Latest Release version: {latestRelease.TagName}");
         if (latestRelease.TagName != currentVersion)
         {
@@ -17,4 +26,6 @@ public static class ManagerUpdater
         Logger.Info("There is no update for Xenia Manager");
         return false;
     }
+
+    #endregion
 }
