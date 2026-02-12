d75741489a0b92cd30ac298a15fe569e26d43a99
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2026-02-12 10:48:06 +0100

    feat: Implemented Xenia Canary UI for `ManagePage`

--- source/XeniaManager.Core/Settings/Sections/BaseSettingsStore.cs
+++ source/XeniaManager.Core/Settings/Sections/BaseSettingsStore.cs
@@ -7,6 +7,12 @@ public abstract class BaseSettingsStore
     [JsonPropertyName("ui")]
     public UiSettings Ui { get; set; } = new UiSettings();
 
+    /// <summary>
+    /// Settings related to the emulator versions
+    /// </summary>
+    [JsonPropertyName("emulators")]
+    public EmulatorSettings Emulator { get; set; } = new EmulatorSettings();
+
     [JsonPropertyName("update_checks")]
     public UpdateCheckSettings UpdateChecks { get; set; } = new UpdateCheckSettings();
 }
--- source/XeniaManager/ViewModels/Pages/ManagePageViewModel.cs
+++ source/XeniaManager/ViewModels/Pages/ManagePageViewModel.cs
@@ -1,6 +1,47 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using XeniaManager.Core.Logging;
+using XeniaManager.Core.Settings;
+using XeniaManager.Core.Utilities;
+
 namespace XeniaManager.ViewModels.Pages;
 
-public class ManagePageViewModel : ViewModelBase
+public partial class ManagePageViewModel : ViewModelBase
 {
-    
+    // Variables
+    private Settings _settings { get; set; }
+
+    // Xenia Canary
+    [ObservableProperty] private bool canaryInstalled;
+    [ObservableProperty] private string canaryVersion = string.Empty;
+    [ObservableProperty] private bool canaryInstall;
+    [ObservableProperty] private bool canaryUninstall;
+    [ObservableProperty] private bool canaryUpdate;
+
+    // Constructor
+    public ManagePageViewModel()
+    {
+        _settings = App.Services.GetRequiredService<Settings>();
+        UpdateEmulatorStatus();
+    }
+
+    // Functions
+    public void UpdateEmulatorStatus()
+    {
+        // Xenia Canary
+        CanaryInstalled = _settings.Settings.Emulator.Canary != null;
+        if (_settings.Settings.Emulator.Canary != null)
+        {
+            CanaryVersion = CanaryInstalled ? _settings.Settings.Emulator.Canary.Version : LocalizationHelper.GetText("ManagePage.Emulator.Manage.Xenia.NotInstalled");
+            Logger.Info<ManagePageViewModel>($"Xenia Canary is installed ({_settings.Settings.Emulator.Canary.Version})");
+        }
+        else
+        {
+            CanaryVersion = LocalizationHelper.GetText("ManagePage.Emulator.Manage.Xenia.NotInstalled");
+            Logger.Info<ManagePageViewModel>("Xenia Canary is not installed");
+        }
+        CanaryInstall = !CanaryInstalled;
+        CanaryUpdate = _settings.Settings.Emulator.Canary is { UpdateAvailable: true };
+        CanaryUninstall = CanaryInstalled;
+    }
 }
