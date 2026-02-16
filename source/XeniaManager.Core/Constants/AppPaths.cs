b2475d163c9be1873b74a99d86ef6f8a067144f0
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2026-02-16 21:25:42 +0100

    Added `GameManager`
    Adds both known and unknown games

--- source/XeniaManager.Core/Constants/AppPaths.cs
+++ source/XeniaManager.Core/Constants/AppPaths.cs
@@ -9,13 +9,17 @@ public class AppPaths
 {
     // Cache
     public static readonly string CacheDirectory = AppPathResolver.GetFullPath("Cache");
-    
+
     // Config
     public static readonly string ConfigDirectory = AppPathResolver.GetFullPath("Config");
     public static readonly string ConfigFile = Path.Combine(ConfigDirectory, "config.json");
-    
+    public static readonly string GameLibraryPath = Path.Combine(ConfigDirectory, "games.json");
+
     // Downloads
     public static readonly string DownloadsDirectory = AppPathResolver.GetFullPath("Downloads");
+    
+    // GameData
+    public static readonly string GameDataDirectory = AppPathResolver.GetFullPath("GameData");
     
     // Logging
     public static readonly string LogsDirectory = AppPathResolver.GetFullPath("Logs");
