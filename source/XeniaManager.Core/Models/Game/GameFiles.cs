aece2e68c3e87412958e5e7831367d52efd1c690
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2026-02-16 09:43:40 +0100

    Game models

---
+++ source/XeniaManager.Core/Models/Game/GameFiles.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+
+namespace XeniaManager.Core.Models.Game;
+
+/// <summary>
+/// Grouping of all file locations related to the game (ISO, patch, configuration, and emulator)
+/// </summary>
+public class GameFiles
+{
+    /// <summary>
+    /// Path to the game's ISO file
+    /// </summary>
+    [JsonPropertyName("game")]
+    public string Game { get; set; }
+
+    /// <summary>
+    /// Path to the game's patch file
+    /// </summary>
+    [JsonPropertyName("patch")]
+    public string? Patch { get; set; }
+
+    /// <summary>
+    /// Path to the game's configuration file
+    /// </summary>
+    [JsonPropertyName("config")]
+    public string? Config { get; set; }
+
+    /// <summary>
+    /// The location of the custom Xenia executable (null if not applicable)
+    /// </summary>
+    [JsonPropertyName("custom_emulator_executable")]
+    public string? CustomEmulatorExecutable { get; set; }
+}
